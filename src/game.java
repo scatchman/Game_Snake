5c40a96c7a9bb8a28482af2248b441676e8a5091
scatchman <abdulahadov@gmail.com>
2015-11-19 18:01:25 +0300

    Add project from Eclipse
    Game version 0.01

---
+++ src/game.java
@@ -0,0 +1,41 @@
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.Terminal;
+
+
+public class game {
+	
+	public static void main (String[] args) {
+		Terminal terminal = TerminalFacade.createTerminal();
+        terminal.enterPrivateMode();
+        //TerminalSize terminalSize = terminal.getTerminalSize();
+        int x = 5;
+        int y = 5;
+        terminal.setCursorVisible(false);
+        terminal.moveCursor(x,y);
+		terminal.putCharacter('@');
+        while(true) {
+        	Key key = terminal.readInput();
+        	if (key != null) {
+        		if (key.getKind() == Key.Kind.ArrowUp) {
+        			y = y - 1;
+        		}
+        		if (key.getKind() == Key.Kind.ArrowDown) {
+        			y = y + 1;
+        		}
+        		if (key.getKind() == Key.Kind.ArrowLeft) {
+        			x = x - 1;
+        		}
+        		if (key.getKind() == Key.Kind.ArrowRight) {
+        			x = x + 1;
+        		}
+        		if (key.getKind() == Key.Kind.Escape) {
+        			System.exit(0);
+        		}
+        		terminal.clearScreen();
+        		terminal.moveCursor(x,y);
+        		terminal.putCharacter('@'); 
+        	}
+		}
+    }
+}
